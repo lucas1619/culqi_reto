f17c50e3a615fc694df74354d27685c860550301
Lucas Moreno <lucas.moreno@credigob.pe>
2023-07-24 15:10:36 -0500

    a

---
+++ src/components/layouts/main.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="w-screen h-screen flex">
+    <sidebar />
+    <div class="lg:w-4/5 w-full">
+      <navbar @toggle-sidebar="toggleSidebar" />
+      <div
+        class="bg-white mx-4 mt-5 rounded-2xl overflow-hidden p-4"
+        style="height: calc(100% - 80px - 2rem)"
+      >
+        <slot></slot>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Navbar from "@/components/general/Navbar.vue";
+import Sidebar from "@/components/general/Sidebar.vue";
+@Options({
+  components: {
+    Navbar,
+    Sidebar,
+  },
+})
+export default class MainLayout extends Vue {
+  openSidebar = false;
+  toggleSidebar() {
+    this.openSidebar = !this.openSidebar;
+  }
+}
+</script>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,118 +1,106 @@
 <template>
-  <main class="w-screen h-screen flex">
-    <sidebar />
-    <div class="lg:w-4/5 w-full">
-      <navbar @toggle-sidebar="toggleSidebar" />
-      <div
-        class="bg-white mx-4 mt-5 rounded-2xl overflow-hidden p-4"
-        style="height: calc(100% - 80px - 2rem)"
-      >
-        <section class="h-full w-full">
-          <section-header title="Empleados" subtitle="Gestiona tus empleados">
-            <template #right-actions>
-              <div class="flex">
-                <secondary-button
-                  class="mr-4 flex w-1/2 lg:w-auto justify-center"
+  <main-layout>
+    <section class="h-full w-full">
+      <section-header title="Empleados" subtitle="Gestiona tus empleados">
+        <template #right-actions>
+          <div class="flex">
+            <secondary-button class="mr-4 flex w-1/2 lg:w-auto justify-center">
+              <file-download-outline class="mr-2" />Descargar
+            </secondary-button>
+            <primary-button class="flex w-1/2 lg:w-auto justify-center">
+              <plus class="mr-2" /> Nuevo
+            </primary-button>
+          </div>
+        </template>
+      </section-header>
+      <div class="h-full">
+        <div class="flex mb-3 lg:mb-6 flex-col lg:flex-row">
+          <Input
+            class="w-full mb-3 lg:mb-0 lg:w-3/4 mt-6 mr-4"
+            type="text"
+            placeholder="Buscar empleado"
+            id="busqueda"
+          />
+          <div class="w-full lg:w-1/4 flex items-end justify-center">
+            <select
+              class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+            >
+              <option disabled selected>Nombre de cargo</option>
+            </select>
+          </div>
+        </div>
+        <div class="overflow-auto h-2/5 lg:h-3/5">
+          <Table :rows="users" :columns="columns">
+            <template #row(name)="{ row }">
+              <p class="font-medium">{{ row.nombre }}</p>
+              <p class="font-normal text-gray-500" style="font-size: 10px">
+                {{ row.correo }}
+              </p>
+            </template>
+            <template #row(cargo)="{ row }">
+              {{ row.cargo }}
+            </template>
+            <template #row(departamento)="{ row }">
+              {{ row.departamento }}
+            </template>
+            <template #row(oficina)="{ row }">
+              {{ row.oficina }}
+            </template>
+            <template #row(cuenta)="{ row }">
+              {{ row.estadoCuenta }}
+            </template>
+            <template #row(acciones)>
+              <div class="flex items-center justify-end">
+                <button
+                  class="w-8 h-8 rounded-lg bg-green-500 text-white mr-2.5 flex items-center justify-center"
                 >
-                  <file-download-outline class="mr-2" />Descargar
-                </secondary-button>
-                <primary-button class="flex w-1/2 lg:w-auto justify-center">
-                  <plus class="mr-2" /> Nuevo
-                </primary-button>
+                  <eye-outline :size="16" />
+                </button>
+                <button
+                  class="w-8 h-8 rounded-lg bg-blue-500 text-white mr-2.5 flex items-center justify-center"
+                >
+                  <pencil-outline :size="16" />
+                </button>
+                <button
+                  class="w-8 h-8 rounded-lg bg-red-500 text-white flex items-center justify-center"
+                >
+                  <trash-can-outline :size="16" />
+                </button>
               </div>
             </template>
-          </section-header>
-          <div class="h-full">
-            <div class="flex mb-3 lg:mb-6 flex-col lg:flex-row">
-              <Input
-                class="w-full mb-3 lg:mb-0 lg:w-3/4 mt-6 mr-4"
-                type="text"
-                placeholder="Buscar empleado"
-                id="busqueda"
-              />
-              <div class="w-full lg:w-1/4 flex items-end justify-center">
-                <select
-                  class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                >
-                  <option disabled selected>Nombre de cargo</option>
-                </select>
-              </div>
-            </div>
-            <div class="overflow-auto h-2/5 lg:h-3/5">
-              <Table :rows="users" :columns="columns">
-                <template #row(name)="{ row }">
-                  <p class="font-medium">{{ row.nombre }}</p>
-                  <p class="font-normal text-gray-500" style="font-size: 10px">
-                    {{ row.correo }}
-                  </p>
-                </template>
-                <template #row(cargo)="{ row }">
-                  {{ row.cargo }}
-                </template>
-                <template #row(departamento)="{ row }">
-                  {{ row.departamento }}
-                </template>
-                <template #row(oficina)="{ row }">
-                  {{ row.oficina }}
-                </template>
-                <template #row(cuenta)="{ row }">
-                  {{ row.estadoCuenta }}
-                </template>
-                <template #row(acciones)>
-                  <div class="flex items-center justify-end">
-                    <button
-                      class="w-8 h-8 rounded-lg bg-green-500 text-white mr-2.5 flex items-center justify-center"
-                    >
-                      <eye-outline :size="16" />
-                    </button>
-                    <button
-                      class="w-8 h-8 rounded-lg bg-blue-500 text-white mr-2.5 flex items-center justify-center"
-                    >
-                      <pencil-outline :size="16" />
-                    </button>
-                    <button
-                      class="w-8 h-8 rounded-lg bg-red-500 text-white flex items-center justify-center"
-                    >
-                      <trash-can-outline :size="16" />
-                    </button>
-                  </div>
-                </template>
-              </Table>
-            </div>
-            <div
-              class="mt-6 flex flex-col items-end lg:flex-row lg:items-center lg:justify-between"
-            >
-              <Pagination :total="50" :perPage="10" />
-              <div class="flex items-center mt-4 lg:mt-0">
-                <span class="text-xs text-gray-600 font-medium mr-6">
-                  Mostrando 1 a 8 de 50 registros
-                </span>
-                <div class="w-fit">
-                  <select
-                    class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
-                  >
-                    <option :value="5">Mostrar 5</option>
-                    <option selected :value="10">Mostrar 10</option>
-                    <option :value="15">Mostrar 15</option>
-                    <option :value="20">Mostrar 20</option>
-                  </select>
-                </div>
-              </div>
+          </Table>
+        </div>
+        <div
+          class="mt-6 flex flex-col items-end lg:flex-row lg:items-center lg:justify-between"
+        >
+          <Pagination :total="50" :perPage="10" />
+          <div class="flex items-center mt-4 lg:mt-0">
+            <span class="text-xs text-gray-600 font-medium mr-6">
+              Mostrando 1 a 8 de 50 registros
+            </span>
+            <div class="w-fit">
+              <select
+                class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
+              >
+                <option :value="5">Mostrar 5</option>
+                <option selected :value="10">Mostrar 10</option>
+                <option :value="15">Mostrar 15</option>
+                <option :value="20">Mostrar 20</option>
+              </select>
             </div>
           </div>
-        </section>
+        </div>
       </div>
-    </div>
-  </main>
+    </section>
+  </main-layout>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { useStore } from "vuex";
-import Avatar from "@/components/general/Avatar.vue";
+import MainLayout from "@/components/layouts/main.vue";
+
 import PrimaryButton from "@/components/general/PrimaryButton.vue";
 import SecondaryButton from "@/components/general/SecondaryButton.vue";
-import Navbar from "@/components/general/Navbar.vue";
 import SectionHeader from "@/components/general/SectionHeader.vue";
 import Plus from "vue-material-design-icons/Plus.vue";
 import FileDownloadOutline from "vue-material-design-icons/FileDownloadOutline.vue";
@@ -121,15 +109,11 @@ import Input from "@/components/general/Input.vue";
 import Table from "@/components/general/Table.vue";
 import { User } from "@/models/user";
 
-import Sidebar from "@/components/general/Sidebar.vue";
-
 import EyeOutline from "vue-material-design-icons/EyeOutline.vue";
 import PencilOutline from "vue-material-design-icons/PencilOutline.vue";
 import TrashCanOutline from "vue-material-design-icons/TrashCanOutline.vue";
 @Options({
   components: {
-    Avatar,
-    Navbar,
     PrimaryButton,
     SecondaryButton,
     Plus,
@@ -137,20 +121,14 @@ import TrashCanOutline from "vue-material-design-icons/TrashCanOutline.vue";
     SectionHeader,
     Input,
     Pagination,
-    Sidebar,
     Table,
     EyeOutline,
     PencilOutline,
     TrashCanOutline,
+    MainLayout,
   },
 })
 export default class MainView extends Vue {
-  openSidebar = false;
-  store = useStore();
-  toggleSidebar() {
-    console.log("toggleSidebar");
-    this.openSidebar = !this.openSidebar;
-  }
   users = [
     {
       id: 287,
@@ -272,15 +250,3 @@ export default class MainView extends Vue {
   ];
 }
 </script>
-<style lang="css" scoped>
-.router-link-exact-active {
-  @apply text-cyan-500;
-}
-.router-link-exact-active .icon {
-  @apply text-cyan-500;
-}
-
-.icon {
-  @apply mr-2 text-gray-500;
-}
-</style>
